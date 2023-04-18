25859f058af13f165134db3eff4c19e3a381741f
Adam Kusiakiewicz <62795338+Papamus@users.noreply.github.com>
2023-04-18 18:07:38 +0200

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,5 @@
+<?php
+//	echo "db";
+	$conn = new mysqli("localhost", "root", "", "cdv_gr_3_v4");
+	$conn = new mysqli("localhost", "testupdate", "test", "cdv_gr_3_v4");
+//	echo $conn->connect_errno; //0 -> ok
